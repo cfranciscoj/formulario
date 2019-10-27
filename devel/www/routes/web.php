f81bd9000535bf8cbf27161974073bb9db68f19e
Carlos Aravena <carlos.aravena@gmail.com>
2019-10-27 13:54:56 -0300

    Se agrega adminlte

--- devel/www/routes/web.php
+++ devel/www/routes/web.php
@@ -22,3 +22,15 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
